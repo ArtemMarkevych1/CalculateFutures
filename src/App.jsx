5a6664f7e9f6364e0a6b56ea8638f4cc9115b76d
amarkevych <aw.markevich@gmail.com>
2023-11-07 11:27:27 +0200

    Add app skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,48 @@
-import React, { useState } from 'react';
-import { Layout, Menu, Switch } from 'antd';
-import { BulbOutlined, CodeSandboxOutlined } from '@ant-design/icons';
+import React, {useState} from 'react';
+import {Layout, Menu, Switch} from 'antd';
+import {BulbOutlined, CodeSandboxOutlined} from '@ant-design/icons';
 import CalculateForm from './components/Form';
 
-const { Header, Content } = Layout;
+const {Header, Content} = Layout;
 
 export default function App() {
-  const [theme, setTheme] = useState('dark');
+    const [theme, setTheme] = useState('dark');
 
-  const handleThemeChange = checked => {
-    const newTheme = checked ? 'dark' : 'light';
-    setTheme(newTheme);
-  };
+    const handleThemeChange = checked => {
+        const newTheme = checked ? 'dark' : 'light';
+        setTheme(newTheme);
+    };
 
-  return (
-    <Layout
-      style={{
-        background: theme === 'dark' ? '' : '#fff'
-      }}
-    >
-      <Header>
-        <Menu theme={theme} mode="horizontal">
-          <Menu.Item key="logo">
-            <CodeSandboxOutlined />
-            Logo
-          </Menu.Item>
-          <Menu.Item key="themeSwitcher" style={{ float: 'right' }}>
-            <BulbOutlined />
-            <Switch
-              checked={theme === 'dark'}
-              onChange={handleThemeChange}
-              checkedChildren="Dark"
-              unCheckedChildren="Light"
-            />
-          </Menu.Item>
-        </Menu>
-      </Header>
-      <Content>
-        <CalculateForm />
-      </Content>
-    </Layout>
-  );
+    const getThemeStyles = (theme, additionalStyles) => {
+        return {
+            ...additionalStyles,
+            background: theme === 'dark' ? '#000' : '',
+            color: theme === 'dark' ? '#fff' : '',
+        }
+    }
+
+    return (
+        <Layout style={getThemeStyles(theme, {minHeight: "100vh", height: "100%"})}>
+            <Header style={getThemeStyles(theme, {padding: 0})}>
+                <Menu theme={theme} mode="horizontal">
+                    <Menu.Item key="logo">
+                        <CodeSandboxOutlined/>
+                        Logo
+                    </Menu.Item>
+                    <Menu.Item key="themeSwitcher" style={{float: 'right'}}>
+                        <BulbOutlined/>
+                        <Switch
+                            checked={theme === 'dark'}
+                            onChange={handleThemeChange}
+                            checkedChildren="Dark"
+                            unCheckedChildren="Light"
+                        />
+                    </Menu.Item>
+                </Menu>
+            </Header>
+            <Content style={getThemeStyles(theme)}>
+                <CalculateForm/>
+            </Content>
+        </Layout>
+    );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,36 +1,42 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
-import { Form, Input, Button } from 'antd'
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Form, Input, Button } from 'antd';
+
+import "../components/Form.css"
 
 export default function CalculateForm() {
-  const { register, handleSubmit } = useForm()
+    const { register, handleSubmit } = useForm();
 
-  const onSubmit = (data) => {
-    // handle form submission
-  }
+    const onSubmit = (data) => {
+        console.log(data);
+    }
 
-  return (
-    <Form onFinish={handleSubmit(onSubmit)}>
-      <Form.Item name="input1">
-        <Input placeholder="Input 1" ref={register} />
-      </Form.Item>
-      <Form.Item name="input2">
-        <Input placeholder="Input 2" ref={register} />
-      </Form.Item>
-      <Form.Item name="input3">
-        <Input placeholder="Input 3" ref={register} />
-      </Form.Item>
-      <Form.Item name="input4">
-        <Input placeholder="Input 4" ref={register} />
-      </Form.Item>
-      <Form.Item name="input5">
-        <Input placeholder="Input 5" ref={register} />
-      </Form.Item>
+    return (
+        <Form onFinish={handleSubmit(onSubmit)} className="form">
+            <Form.Item>
+                <Input {...register('input1')} placeholder="Input 1" />
+            </Form.Item>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit">Submit</Button>
-        <Button type="default" htmlType="reset">Reset</Button>
-      </Form.Item>
-    </Form>
-  )
+            <Form.Item>
+                <Input {...register('input2')} placeholder="Input 2" />
+            </Form.Item>
+
+            <Form.Item>
+                <Input {...register('input3')} placeholder="Input 3" />
+            </Form.Item>
+
+            <Form.Item>
+                <Input {...register('input4')} placeholder="Input 4" />
+            </Form.Item>
+
+            <Form.Item>
+                <Input {...register('input5')} placeholder="Input 5" />
+            </Form.Item>
+
+            <Form.Item className="form-buttons">
+                <Button type="primary" htmlType="submit">Submit</Button>
+                <Button htmlType="reset">Reset</Button>
+            </Form.Item>
+        </Form>
+    );
 }
